ac56600021e220f06f835d0cc2a7d6b52396701d
mohammed afif ahmed <afifahmed456123@gmail.com>
2020-08-23 13:12:16 +0530

    posts files

---
+++ posts/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts/create", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post("http://event-bus-srv:5005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("received event", req.body.type);
+  res.send({});
+});
+
+app.listen(5000, () => {
+  console.log("v30");
+  console.log("listening on port 5000");
+});
